6ba78878928a248c1024bb0902c09cff1675f90c
nananoizy <morihuelacazorla@gmail.com>
2020-01-18 13:48:44 +0100

    Player ship and spash screen

---
+++ Assets/Scripts/levelLoad.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelLoad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("loadNextLevel", 6f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void loadNextLevel(){
+
+        int mainIndex = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.LoadScene(mainIndex + 1);
+    }
+}
