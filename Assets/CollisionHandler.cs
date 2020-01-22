b9b1eacae6f3e315415261419002828e9d5f4810
nananoizy <morihuelacazorla@gmail.com>
2020-01-22 11:10:37 +0100

    Explosion and level reload on death

--- Assets/CollisionHandler.cs
+++ Assets/CollisionHandler.cs
@@ -1,28 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    [SerializeField] float levelLoadDelay = 2f;
+    [SerializeField] GameObject deathExplosion;
     void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     
     void OnTriggerEnter(Collider other){
             StartDeathSequence();
     }
 
     private void StartDeathSequence(){
-        print("player dying");
+        deathExplosion.SetActive(true);
         SendMessage("OnPlayerDeath");
+        Invoke("reloadScene", levelLoadDelay);
+    }
+
+    private void reloadScene(){
+        SceneManager.LoadScene(1);
     }
 }
