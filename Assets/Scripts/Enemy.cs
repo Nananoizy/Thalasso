c32cb9b9abd3fa959ed50cc3cc7ba6cbf31ac609
nananoizy <morihuelacazorla@gmail.com>
2020-01-28 19:19:47 +0100

    UI added

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,21 +7,26 @@ public class Enemy : MonoBehaviour
 
     Collider collider;
     [SerializeField] GameObject enemyDeathFX;
+    [SerializeField] int pointsPerHit = 12;
+
+    ScoreBoard scoreBoard;
 
     // Start is called before the first frame update
     void Start()
     {
         AddNonTriggerBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void OnParticleCollision(GameObject other){
         Instantiate(enemyDeathFX, transform.position, Quaternion.identity);
+        scoreBoard.ScoreHit(pointsPerHit);
         Destroy(gameObject);
     }
 
